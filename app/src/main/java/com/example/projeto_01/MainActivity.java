b6b17b7de165b9f2ae4087e613be7d178ac819b9
douglasmoura <douglasmoura2@outlook.com>
2022-03-26 19:49:21 -0300

    correções de bug

--- app/src/main/java/com/example/projeto_01/MainActivity.java
+++ app/src/main/java/com/example/projeto_01/MainActivity.java
@@ -17,55 +17,54 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void calculadoraIMC(View view){
-
+    public void calculadoraIMC(View view) {
+        TextView msg = findViewById(R.id.TextViewMsg);
         EditText editTextPeso = findViewById(R.id.editTextPeso);
         EditText editTextAltura = findViewById(R.id.editTextAltura);
+        String pesoString = editTextPeso.getText().toString();
+        String alturaString = editTextAltura.getText().toString();
 
-        double peso = Double.parseDouble(String.valueOf(editTextPeso.getText()));
-        double altura = Double.parseDouble(String.valueOf(editTextAltura.getText()));
-        double calculoIMC = imc(peso,altura);
+        int color = Color.argb(250, 250, 0, 0);
 
-        int color = 1;
+        if (pesoString.isEmpty() || alturaString.isEmpty()) {
+            msg.setText("Digite corretamente seu peso e sua altura!");
+        } else {
+            double peso = Double.parseDouble(pesoString);
+            double altura = Double.parseDouble(alturaString);
+            double calculoIMC = imc(peso, altura);
 
-        String resultado= "";
-        TextView msg = findViewById(R.id.TextViewMsg);
-        if (calculoIMC <= 18.5)
-        {
-            resultado= "você está abaixo do peso";
-            color = Color.argb(250,200,200,0);
-        }
-        else if (calculoIMC > 18.5 && calculoIMC <= 24.9)
-        {
-            resultado="você está com peso normal";
-            color = Color.argb(250,0,200,15);
-        }
-        else if (calculoIMC >= 25 && calculoIMC <= 29.9)
-        {
-            resultado="você está com sobrepeso";
-            color = Color.argb(250,200,85,0);
-        }
-        else if (calculoIMC >= 30 && calculoIMC <= 34.9)
-        {
-            resultado="você está com obsidade Grau I";
-            color = Color.argb(250,250,30,30);
-        }
-        else if (calculoIMC >= 35 && calculoIMC <= 39.9)
-        {
-            resultado="você está com obsidade Grau II";
-            color = Color.argb(250,204,0,0);
-        }
-        else if (calculoIMC >= 40)
-        {
-            resultado="você está com obsidade Grau III";
-            color = Color.argb(250,153,0,0);
+            String resultado = "";
+            if (peso <= 0 || altura <= 0) {
+                msg.setText("Digite corretamente seu peso e sua altura!");
+
+            }else{
+                if (calculoIMC <= 18.5) {
+                    resultado = "você está abaixo do peso";
+                    color = Color.argb(250, 200, 200, 0);
+                } else if (calculoIMC > 18.5 && calculoIMC <= 24.9) {
+                    resultado = "você está com peso normal";
+                    color = Color.argb(250, 0, 200, 15);
+                } else if (calculoIMC >= 25 && calculoIMC <= 29.9) {
+                    resultado = "você está com sobrepeso";
+                    color = Color.argb(250, 200, 85, 0);
+                } else if (calculoIMC >= 30 && calculoIMC <= 34.9) {
+                    resultado = "você está com obsidade Grau I";
+                    color = Color.argb(250, 250, 30, 30);
+                } else if (calculoIMC >= 35 && calculoIMC <= 39.9) {
+                    resultado = "você está com obsidade Grau II";
+                    color = Color.argb(250, 204, 0, 0);
+                } else if (calculoIMC >= 40) {
+                    resultado = "você está com obsidade Grau III";
+                    color = Color.argb(250, 153, 0, 0);
+                }
+
+                msg.setText(String.format("Seu IMC é %.2f\n\n %s", calculoIMC, resultado));
+            }
         }
         msg.setTextColor(color);
-
-
-        msg.setText(String.format("Seu IMC é %.2f\n\n %s", calculoIMC, resultado));
     }
-    public double imc(double peso, double altura){
+
+    public double imc(double peso, double altura) {
         double imc = peso / (altura * altura);
         return imc;
     }
